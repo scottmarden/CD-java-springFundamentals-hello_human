78a213e1ecb31e277517634d01fed597d81f2231
Scott <marden.scott@gmail.com>
2017-08-07 17:47:34 -0700

    Initial Commit, basic assignment practicing routing and grabbing information from url paramaters

---
+++ src/main/java/com/scottmarden/hellohuman/controllers/HomeCtrl.java
@@ -0,0 +1,17 @@
+package com.scottmarden.hellohuman.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeCtrl {
+	
+	@RequestMapping("/")
+	public String index(Model model, @RequestParam(value="firstName", required=false, defaultValue="Human") String firstName, @RequestParam(value="lastName", required=false, defaultValue="") String lastName) {
+		model.addAttribute("firstName", firstName);
+		model.addAttribute("lastName", lastName);
+		return "index.jsp";
+	}	
+}
---
+++ src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Home</title>
+</head>
+<body>
+	<h1>Hello <c:out value="${firstName}" /> <c:out value="${lastName}" /> </h1>
+	<h4>Welcome to Spring Boot! I think you'll like it here!</h4>
+</body>
+</html>
